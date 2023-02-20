3f4d3c4c6c216f9f9e090d147d5aed1690d51796
wesley-foussard <tktrekk@gmail.com>
2023-02-20 15:38:37 +0100

    create ok

---
+++ migration/revert/1.create_tables.sql
@@ -0,0 +1,7 @@
+-- Revert fitwork:1.create_tables from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ migration/verify/1.create_tables.sql
@@ -0,0 +1,7 @@
+-- Verify fitwork:1.create_tables on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
