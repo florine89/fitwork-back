f666cf7c59056a8e6029e73ac9f44d70d174d81f
wesley-foussard <53617379+nimiteos-it@users.noreply.github.com>
2023-03-01 15:19:33 +0100

    replace slug bu type in requests

--- migration/revert.sh
+++ migration/revert.sh
@@ -3,5 +3,5 @@ export PGPASSWORD=fitwork
 export PGHOST=localhost
 #sqitch revert 1.create_tables
 #sqitch revert 2.alter_mail
-#sqitch revert 3.delete+casccade
-sqitch revert 4.cascade_article
+sqitch revert 3.delete+gender
+#sqitch revert 4.cascade_article
