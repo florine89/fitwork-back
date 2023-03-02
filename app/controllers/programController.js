109e47fc9c3a3649d68745bdeb4b3e59909b94f7
BarreAlexandre <barre.alexandre.iso@gmail.com>
2023-03-02 15:49:23 +0100

    delete Ok

--- app/controllers/programController.js
+++ app/controllers/programController.js
@@ -28,42 +28,24 @@ export default{
                 res.json(postArticleInProgram);
             }
             catch (error) {
-                next (error);
-        }
-    },
-
-    async postArticleProgram(req,res,next){
-        const userId = req.body.user_id
-        const articleId = req.params.id
-        try{
-            const postArticleInProgram = await programMapper.addToProgram(userId,articleId);
-            console.log(postArticleInProgram)
-            if(!postArticleInProgram){
-                throw "Impossible de rajouter l'article au programme."
-            }
-                res.json(postArticleInProgram);
-            }
-            catch (error) {
+                console.log('postUSerProgram-error : ', error);
                 next (error);
         }
     },
     
     async deleteArticleProgram(req,res,next){
-        const userId = req.body.user_id
-        const articleId = req.params.id
         try{
-            const postArticleInProgram = await programMapper.addToProgram(userId,articleId);
-            console.log(postArticleInProgram)
-            if(!postArticleInProgram){
-                throw "Impossible de rajouter l'article au programme."
+            const deleted = await programMapper.deleteFromProgram(req.params.id);
+            if(!deleted){
+                throw "Impossible de supprimer l'article au programme."
             }
-                res.json(postArticleInProgram);
+                res.json("deleted");
             }
-            catch (error) {
-                next (error);
-            }
+        catch(error) {
+            console.log('deleteUSerProgram-error : ', error);
+            next (error);
+        }
     }
-};
-
+}
 
 
