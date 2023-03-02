4ec587768f7a8c30b57c9b57b0dba683319f5910
wesley-foussard <53617379+nimiteos-it@users.noreply.github.com>
2023-03-02 14:42:55 +0100

    id added to program table

--- migration/verify/2.alter_mail.sql
+++ migration/verify/2.alter_mail.sql
@@ -2,6 +2,12 @@
 
 BEGIN;
 
--- XXX Add verifications here.
-
+SELECT conname
+       FROM pg_catalog.pg_constraint con
+            INNER JOIN pg_catalog.pg_class rel
+                       ON rel.oid = con.conrelid
+            INNER JOIN pg_catalog.pg_namespace nsp
+                       ON nsp.oid = connamespace
+       WHERE nsp.nspname = 'public'
+             AND rel.relname = 'user';
 ROLLBACK;
