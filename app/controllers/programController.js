f9d324b225653905ae3c0b0bce0c4be1a2f8675f
BarreAlexandre <113354379+BarreAlexandre@users.noreply.github.com>
2023-03-02 14:18:30 +0100

    Dev (#66)
    * security middleware written
    
    * update login with role
    
    * addToProgram probably ok (#65)
    
    ---------
    
    Co-authored-by: wesley-foussard <53617379+nimiteos-it@users.noreply.github.com>
    Co-authored-by: Nimiteos <53617379+wesley-foussard@users.noreply.github.com>

--- app/controllers/programController.js
+++ app/controllers/programController.js
@@ -14,6 +14,22 @@ export default{
                 console.log('getUSerProgram-error : ', error);
                 next (error);
         }
+    },
+
+    async postArticleProgram(req,res,next){
+        const userId = req.body.user_id
+        const articleId = req.params.id
+        try{
+            const postArticleInProgram = await programMapper.addToProgram(userId,articleId);
+            console.log(postArticleInProgram)
+            if(!postArticleInProgram){
+                throw "Impossible de rajouter l'article au programme."
+            }
+                res.json(postArticleInProgram);
+            }
+            catch (error) {
+                next (error);
+        }
     }
 }
 
